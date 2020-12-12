94db0dc65b94260da9ae2eeb8f99444b17063a6d
HMoreira2020 <moreiraheather@gmail.com>
2020-12-11 21:47:12 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,39 @@
-export default (state = [], action) => {
-  return state;
+
+
+export default function manageQuotes(state = [], action) {
+  let idx
+  let currentQuote
+  switch (action.type) {
+    case 'ADD_QUOTE': 
+      return [...state, action.quote]
+
+    case 'REMOVE_QUOTE': 
+      idx = state.findIndex(quote => quote.id === action.quoteId)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)]
+
+    case 'UPVOTE_QUOTE':
+      idx = state.findIndex(quote => quote.id === action.quoteId)
+      currentQuote = state.find(quote => quote.id === action.quoteId)
+      return [
+        ...state.slice(0, idx), 
+          Object.assign({}, currentQuote, { votes: currentQuote.votes += 1}), 
+          ...state.slice(idx + 1)
+        ]
+
+    
+    case 'DOWNVOTE_QUOTE':
+      idx = state.findIndex(quote => quote.id === action.quoteId)
+      currentQuote = state.find(quote => quote.id === action.quoteId)
+      if (currentQuote.votes > 0) {
+        return [
+          ...state.slice(0, idx),
+          Object.assign({}, currentQuote, { votes: currentQuote.votes -= 1}), 
+          ...state.slice(idx + 1)
+        ]
+      }
+      return state
+        
+    default: 
+      return state
+  }
 }
