69f468804966d91c3ff6192cecc7b44f4fba39b0
EvanTheSmith <ProfessionalEmail@Zoho.com>
2021-01-10 05:11:13 -0600

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,22 @@
 export default (state = [], action) => {
-  return state;
+
+  let idx; let newState = [...state];
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.quote];
+    case 'REMOVE_QUOTE':
+      idx = state.findIndex(quote => quote.id === action.quoteId)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+    case 'UPVOTE_QUOTE':
+      idx = newState.find(quote => quote.id === action.quoteId);
+      idx.votes += 1;
+      return newState;
+    case 'DOWNVOTE_QUOTE':
+      idx = newState.find(quote => quote.id === action.quoteId);
+      if(idx.votes>0){idx.votes -= 1;}
+      return newState;
+    default:
+      return state;
+  }
+
 }
